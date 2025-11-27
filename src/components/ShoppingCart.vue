ab0452a3111bc66c72f4959c580fd71a95f30a0b
mosokni <84025406+mosokni@users.noreply.github.com>
2025-11-27 15:35:03 +0000

    Implement shopping cart component

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <div v-if="cart.length === 0">
+        Cart is empty
+      </div>
+  
+      <div v-else>
+        <div v-for="(lesson, index) in cart" :key="index">
+          <p>{{ lesson.subject }} - £{{ lesson.price }}</p>
+          <button @click="$emit('remove-from-cart', index)">Remove</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['cart']
+  }
+  </script>
+  

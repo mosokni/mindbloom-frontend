ad4613bda1e8131726873049dc4ea5c0830b6dca
mosokni <84025406+mosokni@users.noreply.github.com>
2025-11-27 15:34:24 +0000

    Set up base Vue application structure

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/lessons">Lessons</router-link> |
+      <router-link to="/cart">Cart ({{ cart.length }})</router-link>
+    </nav>
+
+    <router-view
+      :cart="cart"
+      @add-to-cart="addToCart"
+      @remove-from-cart="removeFromCart"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      cart: []
+    }
+  },
+  methods: {
+    addToCart(lesson) {
+      this.cart.push(lesson)
+    },
+    removeFromCart(index) {
+      this.cart.splice(index, 1)
+    }
+  }
+}
+</script>
